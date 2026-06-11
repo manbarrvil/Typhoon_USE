bf73e683f3dab7df41d085e506b7bc93cdd3c5f5
manbarrvil <manbarrvil@gmail.com>
2026-06-11 08:41:12 +0200

    API HIL

--- HIL_API/first_model_python.py
+++ HIL_API/first_model_python.py
@@ -21,15 +21,33 @@ hil.set_contactor('Plant.S1', swControl=True, swState=False, executeAt=None)   #
 hil.set_contactor('Plant.S1', swControl=True, swState=True,  executeAt=execute_at)  # cerrar a los close_at s
 print(f"Contactor Plant.S1 programado para cerrar a t={execute_at:.4f} s  (sim_step={sim_step} s)")
 
+
+startAC_triggered  = False
+i_dq_ref_triggered = False
+
 try:
     print("Leyendo señales... Pulsa Ctrl+C para detener.")
     while True:
         sim_time    = hil.get_sim_time()
         voltage_val = hil.read_analog_signal(name="Plant.VDC")
-        # software_value refleja el último comando enviado, no el estado actual del circuito.
-        # Inferimos el estado real a partir del tiempo de simulación.
-        contactor_state = "Closed" if sim_time >= execute_at else "Opened"
-        print(f"t={sim_time:.2f} s  |  Tensión = {voltage_val} V  |  Plant.S1 = {contactor_state}")
+        is_d_ref    = hil.read_analog_signal(name="SCADA.is_d_ref")
+        scada_id    = hil.read_analog_signal(name="SCADA.id")
+        is_q_ref    = hil.read_analog_signal(name="SCADA.is_q_ref")
+        scada_iq    = hil.read_analog_signal(name="SCADA.iq")
+        contactor_state = "Closed" if scada_id != 0.0 else "Opened"
+
+        if not startAC_triggered and sim_time >= 1.1:
+            hil.set_scada_input_value('SCADA.startAC', 1)
+            startAC_triggered = True
+            print(f"t={sim_time:.2f} s  |  SCADA Input 'startAC' → 1")
+
+        if startAC_triggered and not i_dq_ref_triggered and sim_time >= 1.5:
+            hil.set_scada_input_value('SCADA.i_d_ref', 10)
+            hil.set_scada_input_value('SCADA.i_q_ref', -10.0)
+            i_dq_ref_triggered = True
+            print(f"t={sim_time:.2f} s  |  SCADA Input 'i_d_ref' → 10 A  |  'i_q_ref' → -10.0 A")
+
+        print(f"t={sim_time:.2f} s  |  Tensión = {voltage_val:.2f} V  |  SCADA.is_d_ref = {is_d_ref:.2f} A  |  SCADA.id = {scada_id:.2f}  |  SCADA.is_q_ref = {is_q_ref:.2f} A  |  SCADA.iq = {scada_iq:.2f}  |  Plant.S1 = {contactor_state}")
         time.sleep(1)
 
 except KeyboardInterrupt:
